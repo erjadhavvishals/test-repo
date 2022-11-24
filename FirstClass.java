806f0f45518b0c2f3c0b19be4928ca46a486f6b9
Vishal Jadhav <er.jadhavvishals@gmail.com>
2022-11-24 16:50:35 +0530

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,8 @@
+class FirstClass {
+    public static void main(String args[]) {
+        System.out.println("First Class!");
+        System.out.println("This file is changed");
+        System.out.println("Skip Staging area, test");
+        System.out.println("Skip Staging area, test");
+    }
+}
